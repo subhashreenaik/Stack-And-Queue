f4d03dcb1e756809158060194f4b9208883d7ea5
Subhashree naik <subhashree.naik@gmail.com>
2022-03-03 20:57:20 +0530

    Creating stack using linked list

--- src/com/assignment/blz/LinkedList.java
+++ src/com/assignment/blz/LinkedList.java
@@ -1,10 +1,30 @@
 package com.assignment.blz;
 
-public class LinkedList {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        System.out.println("Welcome to stack and queues program ");
+
+public class LinkedList {
+	/**
+	* push - this method is creating to add data to linkedList
+	* 
+	*/
+	Node head;
+	public void push(int data) {
+		Node new_node = new Node(data);
+        new_node.next = head;
+        head = new_node;
 	}
+	
+	public void print() {
+        if (head == null)
+          System.out.println("No elements to display");
+        else {
+          Node temp = head;
+          while (temp.next != null) {
+          System.out.print(temp.data + " -> ");
+          temp = temp.next;
+          }
+          System.out.println(temp.data);
+        }
+    }
 
 }
