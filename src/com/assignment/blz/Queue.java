9b9e023fe13fab4baaf0ec71a29662bb25215bb1
Subhashree naik <subhashree.naik@gmail.com>
2022-03-03 22:32:48 +0530

    Dequeuing elements from queue

--- src/com/assignment/blz/Queue.java
+++ src/com/assignment/blz/Queue.java
@@ -9,6 +9,7 @@ public class Queue {
 		* PROCEDURE
 		* 1. Creating Linked list
 		  2. enqueuing to the queue
+		  3. Dequeing element from queue
 		*/
 		
 		/*
@@ -22,8 +23,16 @@ public class Queue {
 		list.append(56);
 		list.append(30);
 		list.append(70);
-		
 		list.print(list.head);
+		
+		/*
+		 * 3. dequeuing to the queue
+		 */
+		Node x= list.dequeue(list.head);
+	    list.print(list.dequeue(list.head));
+		
+		
+		
 
 }
 }
