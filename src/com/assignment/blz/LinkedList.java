868b572cfdc1c05df9e0a7c8592726da8838ea71
Subhashree naik <subhashree.naik@gmail.com>
2022-03-03 20:38:41 +0530

    welcome to stack and queue

---
+++ src/com/assignment/blz/LinkedList.java
@@ -0,0 +1,10 @@
+package com.assignment.blz;
+
+public class LinkedList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Welcome to stack and queues program ");
+	}
+
+}
